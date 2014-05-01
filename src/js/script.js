90bbe7a8b06eff829d89b66c67e188af52eccb4e
Yoeori <yoeori@live.nl>
2014-05-01 19:06:14 +0200

    organizing layout.

--- src/js/script.js
+++ src/js/script.js
@@ -1,28 +1,19 @@
 var citymap = {};
 citymap['chicago'] = {
-  center: new google.maps.LatLng(41.878113, -87.629798),
-  population: 2842518
-};
-citymap['newyork'] = {
-  center: new google.maps.LatLng(40.714352, -74.005973),
-  population: 8143197
-};
-citymap['losangeles'] = {
-  center: new google.maps.LatLng(34.052234, -118.243684),
-  population: 3844829
+  center: new google.maps.LatLng(52.1497977,5.3984084,17),
+	radius: 10
 };
 var cityCircle;
 
 function initialize() {
   // Create the map.
   var mapOptions = {
-    zoom: 4,
-    center: new google.maps.LatLng(37.09024, -95.712891),
-    mapTypeId: google.maps.MapTypeId.TERRAIN
+    zoom: 14,
+    center: new google.maps.LatLng(52.1497977, 5.3984084,17),
+	disableDefaultUI: true
   };
 
-  var map = new google.maps.Map(document.getElementById('map-canvas'),
-      mapOptions);
+  var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 
   // Construct the circle for each value in citymap.
   // Note: We scale the population by a factor of 20.
@@ -35,7 +26,7 @@ function initialize() {
       fillOpacity: 0.35,
       map: map,
       center: citymap[city].center,
-      radius: citymap[city].population / 20
+      radius: citymap[city].radius
     };
     // Add the circle for this city to the map.
     cityCircle = new google.maps.Circle(populationOptions);
